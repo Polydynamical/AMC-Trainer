af71f9f0f5cabfe1da5fb177cf3e6b8a685088f6
cbracketdash <cbracketdash@gmail.com>
2021-03-13 15:33:07 -0800

    AMC TRAINER SO OP

---
+++ js/draw.js
@@ -0,0 +1,124 @@
+var canvas, ctx, flag = false,
+prevX = 0,
+currX = 0,
+prevY = 0,
+currY = 0,
+dot_flag = false;
+
+var x = "black",
+y = 2;
+
+function init() {
+    canvas = document.getElementById("can");
+    ctx = canvas.getContext("2d");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    can.style.left = "0px";
+    can.style.top = "0px";
+    w = canvas.width;
+    h = canvas.height;
+
+    canvas.addEventListener("mousemove", function (e) {
+        findxy('move', e)
+    }, false);
+    canvas.addEventListener("mousedown", function (e) {
+        findxy('down', e)
+    }, false);
+    canvas.addEventListener("mouseup", function (e) {
+        findxy('up', e)
+    }, false);
+    canvas.addEventListener("mouseout", function (e) {
+        findxy('out', e)
+    }, false);
+}
+
+function toggle() {
+    if (document.getElementById("draw").style.display == "none") {
+        document.getElementById('draw').style.display='block';
+        init();
+    } else {
+        document.getElementById('draw').style.display='none';
+    }
+}
+
+function color(obj) {
+    switch (obj.id) {
+        case "green":
+            x = "green";
+            break;
+        case "blue":
+            x = "blue";
+            break;
+        case "red":
+            x = "red";
+            break;
+        case "yellow":
+            x = "yellow";
+            break;
+        case "orange":
+            x = "orange";
+            break;
+        case "black":
+            x = "black";
+            break;
+        case "white":
+            x = "white";
+            break;
+    }
+    if (x == "white") y = 14;
+    else y = 3;
+
+}
+
+function draw() {
+    ctx.beginPath();
+    ctx.moveTo(prevX, prevY);
+    ctx.lineTo(currX, currY);
+    ctx.strokeStyle = x;
+    ctx.lineWidth = y;
+    ctx.stroke();
+    ctx.closePath();
+}
+
+function erase() {
+    var m = confirm("Are you sure you want to clear?");
+    if (m) {
+        ctx.clearRect(0, 0, w, h);
+    }
+}
+
+function findxy(res, e) {
+if (res == 'down') {
+    prevX = currX;
+    prevY = currY;
+    currX = e.clientX - canvas.offsetLeft;
+    currY = e.clientY - canvas.offsetTop;
+
+    flag = true;
+    dot_flag = true;
+    if (dot_flag) {
+        ctx.beginPath();
+        ctx.fillStyle = x;
+        ctx.fillRect(currX, currY, 2, 2);
+        ctx.closePath();
+        dot_flag = false;
+    }
+}
+if (res == 'up' || res == "out") {
+    flag = false;
+}
+if (res == 'move') {
+    if (flag) {
+        prevX = currX;
+        prevY = currY;
+        currX = e.clientX - canvas.offsetLeft;
+        currY = e.clientY - canvas.offsetTop;
+        draw();
+    }
+}
+}
+function resize_canvas(){
+        canvas.width  = window.innerWidth;
+        canvas.height = window.innerHeight;
+            
+}
