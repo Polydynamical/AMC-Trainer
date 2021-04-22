a2da086a0c3f6d867b5b1b00e112c494dac9452f
cbracketdash <cbracketdash@gmail.com>
2021-04-21 21:38:31 -0700

    continue code cleanup

--- js/script.js
+++ js/script.js
@@ -1,3 +1,39 @@
+
+document.addEventListener('contextmenu', event => event.preventDefault());
+document.onkeydown = function(e) {
+
+
+    if(event.keyCode == 123) {
+    return false;
+    }
+    if(e.ctrlKey && e.keyCode == 'E'.charCodeAt(0)){
+    return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+    return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+    return false;
+    }
+    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+    return false;
+    }
+    if(e.ctrlKey && e.keyCode == 'S'.charCodeAt(0)){
+    return false;
+    }
+    if(e.ctrlKey && e.keyCode == 'H'.charCodeAt(0)){
+    return false;
+    }
+    if(e.ctrlKey && e.keyCode == 'A'.charCodeAt(0)){
+    return false;
+    }
+    if(e.ctrlKey && e.keyCode == 'F'.charCodeAt(0)){
+    return false;
+    }
+    if(e.ctrlKey && e.keyCode == 'E'.charCodeAt(0)){
+    return false;
+    }
+}
             function logo() {
                 var color = document.getElementById("logoColor").value 
                 switch (color) {
@@ -251,7 +287,7 @@
                 request(answer_key_link, handleAns);
 
                 document.getElementById("problem_id").innerHTML = problem_id;
-                document.getElementById("check_ans").style.display = "block";
+                document.getElementById("check_ans").style.display = "flex";
                 textc();
 
             }
