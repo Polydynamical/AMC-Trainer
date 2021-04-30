2737fb76a3eb3aae6eecc28161bd92adb4d621c4
cbracketdash <cbracketdash@gmail.com>
2021-04-29 19:16:33 -0700

    fix stuff

--- js/script.js
+++ js/script.js
@@ -1,39 +1,6 @@
-document.addEventListener('contextmenu', event => event.preventDefault());
 document.onkeydown = function(e) {
-
-
-    if(event.keyCode == 123) {
-        return false;
-    }
-    if(e.ctrlKey && e.keyCode == 'E'.charCodeAt(0)){
-        return false;
-    }
-    if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
-        return false;
-    }
-    if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
-        return false;
-    }
-    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
-        return false;
-    }
     if(e.ctrlKey && e.keyCode == 13){
         get_new_problem();
-    }
-    if(e.ctrlKey && e.keyCode == 'S'.charCodeAt(0)){
-        return false;
-    }
-    if(e.ctrlKey && e.keyCode == 'H'.charCodeAt(0)){
-        return false;
-    }
-    if(e.ctrlKey && e.keyCode == 'A'.charCodeAt(0)){
-        return false;
-    }
-    if(e.ctrlKey && e.keyCode == 'F'.charCodeAt(0)){
-        return false;
-    }
-    if(e.ctrlKey && e.keyCode == 'E'.charCodeAt(0)){
-        return false;
     }
 }
             function logo() {
