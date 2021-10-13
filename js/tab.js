932189fcfd3013f4111da7c06f8f30864c666ec5
Polydynamical <cbracketdash@gmail.com>
2021-10-13 14:15:44 -0700

    this just rearranges where everything is (no need for pr)

--- js/tab.js
+++ js/tab.js
@@ -6,7 +6,15 @@ const streak = document.getElementById("streakModal");
 
 document.getElementsByClassName("tabButton")[0].click();
 
-function changeTab(evt, tabName) { // skipcq: JS-0239
+function changeTab(obj, evt, tabName) { // skipcq: JS-0239
+    const all = document.querySelectorAll(".tabButton");
+    for(var i = 0;i < all.length; i++){
+        all[i].style.background = 'transparent';
+        all[i].style.color = '#426696';
+    }
+    obj.style.backgroundImage = 'linear-gradient(to right top, #63b7dd, #63ddc7)';
+    obj.style.color = 'white';
+   
     const x = document.getElementsByClassName("tab");
     for (let i = 0; i < x.length; i++) {
         x[i].style.display = "none";
